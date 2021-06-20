25ae62fe5c0a6e0eb897e02d263de81b2940f6f0
Ivan Khokhlov <khokhlov.ivan@gmail.com>
2021-06-20 17:45:23 +0200

    Moved code to the Notebook

--- prepare.py
+++ prepare.py
@@ -11,7 +11,6 @@ filepath = os.getcwd() + '/event_data'
 
 # Create a for loop to create a list of files and collect each filepath
 for root, dirs, files in os.walk(filepath):
-    
 # join the file path and roots with the subdirectories using glob
     file_path_list = glob.glob(os.path.join(root,'*'))
     #print(file_path_list)
